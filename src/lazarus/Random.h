1f4ce3358c805afb854cff5db83864ec80ab9913
monzo94 <dmonzonis@protonmail.com>
2019-03-23 11:14:24 +0100

    Add more random methods

--- src/lazarus/Random.h
+++ src/lazarus/Random.h
@@ -16,12 +16,20 @@ public:
     static void seed();
 
     /**
-    * Return a random integer between the two given numbers, both ends included.
+    * Return a random integer between the two given numbers with equal probability.
     * 
     * If a < b, then it will return an integer in [a, b].
-    * Otherwise, it will be between [b, a].
+    * Otherwise, it will be in [b, a].
     */
     static long range(long a, long b);
+
+    /**
+     * Return a random floating point between the two given numbers with equal probability.
+     * 
+     * If a < b, then it will return a double in [a, b).
+     * Otherwise, it will be in [b, a).
+     */
+    static double rangeReal(double a, double b);
 
     /**
      * Rolls a dice with the specified number of sides a certain number of times
@@ -33,6 +41,17 @@ public:
      * Return true with a 1 in n probability.
      */
     static bool oneIn(unsigned n);
+
+    /**
+     * Return a random number generated from a normal distribution.
+     * 
+     * The normal (or Gaussian) distribution will use the given mean and
+     * standard deviation.
+     * 
+     * @param mean The mean of the distribution.
+     * @param stdev The standard deviation of the distribution.
+     */
+    static double normal(double mean, double stdev);
 
 private:
     static std::mt19937 generator;
