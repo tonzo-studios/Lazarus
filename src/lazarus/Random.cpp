e8531284cae1582b541c4faca118f83029cca8e9
monzo94 <dmonzonis@protonmail.com>
2019-03-23 12:49:06 +0100

    Make range functions generic

--- src/lazarus/Random.cpp
+++ src/lazarus/Random.cpp
@@ -24,24 +24,6 @@ void Random::seed()
     }
 }
 
-long Random::range(long a, long b)
-{
-    if (a > b)
-        std::swap(a, b);
-
-    std::uniform_int_distribution<long> dist(a, b);
-    return dist(generator);
-}
-
-double Random::rangeReal(double a, double b)
-{
-    if (a > b)
-        std::swap(a, b);
-
-    std::uniform_real_distribution<double> dist(a, b);
-    return dist(generator);
-}
-
 ulong Random::roll(unsigned sides, unsigned times)
 {
     if (sides < 2 || times < 1)
@@ -57,7 +39,7 @@ bool Random::oneIn(unsigned n)
 {
     if (n < 2)
         return true;
-    return Random::range(1, n) == 1;
+    return Random::range(static_cast<unsigned>(1), n) == 1;
 }
 
 double Random::normal(double mean, double stdev)
