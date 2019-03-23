995f7d687926e50cd3a236d194cf2f6feee9eeba
monzo94 <dmonzonis@protonmail.com>
2019-03-23 20:19:13 +0100

    Add LazarusException class

--- src/lazarus/common.h
+++ src/lazarus/common.h
@@ -1,6 +1,20 @@
+#pragma once
+
+#include <stdexcept>
+
 #ifndef NDEBUG
 #include <cstdio>
 #define DEBUG(...) do { printf(__VA_ARGS__); printf("\n"); } while (0)
 #else
 #define DEBUG(...)
 #endif
+
+class LazarusException : std::runtime_error
+{
+public:
+  LazarusException(const std::string &msg);
+  const char *c_str() const noexcept
+  {
+      return what();
+  }
+};
