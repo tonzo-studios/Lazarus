79da2391b1cf1f9ab06c0cf13c72026fee219e08
monzo94 <dmonzonis@protonmail.com>
2019-03-24 21:24:00 +0100

    typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ cmake -DCMAKE_BUILD_TYPE="$build_type" -DSFML_DIR=./SFML/install/lib/cmake/SFML 
     -DSFML_INCLUDE_DIR=./SFML/install/include ..
 make -j3
 cd tests
-[ -f lazarus_tests ] && sh -c ./lazarus_tests
+[ -f lazarus_test ] && sh -c ./lazarus_test
 
 # Build documentation
 cd ../..
