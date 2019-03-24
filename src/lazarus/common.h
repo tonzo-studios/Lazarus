0b91d04bdcc237230a58ed94c65019492edd50c8
Daniel Monzonís <monzo94@users.noreply.github.com>
2019-03-24 10:05:59 +0100

    Add own exception class (#7)
    * Add LazarusException class
    
    * Complete and document exception class

--- src/lazarus/common.h
+++ src/lazarus/common.h
@@ -1,6 +1,32 @@
+#pragma once
+
+#include <stdexcept>
+
 #ifndef NDEBUG
 #include <cstdio>
 #define DEBUG(...) do { printf(__VA_ARGS__); printf("\n"); } while (0)
 #else
 #define DEBUG(...)
 #endif
+
+/**
+ * Class for exceptions related to the Lazarus engine.
+ * 
+ * Meant only for internal use.
+ */
+namespace __lz
+{    
+class LazarusException : public std::runtime_error
+{
+public:
+  LazarusException(const std::string &msg)
+      : std::runtime_error(msg)
+  {
+  }
+
+  const char *c_str() const noexcept
+  {
+      return what();
+  }
+};
+}
