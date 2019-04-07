cb5aad6c8c46d8d1aa6cac3eb91df497449be56a
monzo94 <dmonzonis@protonmail.com>
2019-04-07 15:29:36 +0200

    Update module header

--- src/lazarus/ECS.h
+++ src/lazarus/ECS.h
@@ -1,5 +1,6 @@
 #pragma once
 
-// #include <lazarus/ECS/ECSEngine.h>
+#include <lazarus/ECS/ECSEngine.h>
 #include <lazarus/ECS/Entity.h>
 #include <lazarus/ECS/EntityHolder.h>
+#include <lazarus/ECS/System.h>
