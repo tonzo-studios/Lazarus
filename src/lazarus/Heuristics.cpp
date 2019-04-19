896b81309be131e7f472d7b0d9ba128e05d02b5c
monzo94 <dmonzonis@protonmail.com>
2019-04-19 12:31:48 +0200

    Use floats instead of doubles

--- src/lazarus/Heuristics.cpp
+++ src/lazarus/Heuristics.cpp
@@ -2,26 +2,26 @@
 
 using namespace lz;
 
-double manhattanDistance(const Position2D& a, const Position2D& b)
+float manhattanDistance(const Position2D& a, const Position2D& b)
 {
     return std::abs(a.x - b.x) + std::abs(a.y - b.y);
 }
 
-double euclideanDistance(const Position2D& a, const Position2D& b)
+float euclideanDistance(const Position2D& a, const Position2D& b)
 {
     int dx = a.x - b.x,
         dy = a.y - b.y;
     return std::sqrt(dx * dx + dy * dy);
 }
 
-double chebyshevDistance(const Position2D& a, const Position2D& b)
+float chebyshevDistance(const Position2D& a, const Position2D& b)
 {
     return std::max(std::abs(a.x - b.x), std::abs(a.y - b.y));
 }
 
-double octileDistance(const Position2D& a, const Position2D& b)
+float octileDistance(const Position2D& a, const Position2D& b)
 {
-    double dx = std::abs(a.x - b.x);
-    double dy = std::abs(a.y - b.y);
+    float dx = std::abs(a.x - b.x);
+    float dy = std::abs(a.y - b.y);
     return std::sqrt(2) * std::min(dx, dy) + std::abs(dx - dy);
 }
--- src/lazarus/Heuristics.h
+++ src/lazarus/Heuristics.h
@@ -9,20 +9,20 @@ namespace lz
 /**
  * Compute the Manhattan distance between two 2D positions.
  */
-double manhattanDistance(const Position2D&, const Position2D&);
+float manhattanDistance(const Position2D&, const Position2D&);
 
 /**
  * Compute the Euclidean distance between two 2D positions.
  */
-double euclideanDistance(const Position2D&, const Position2D&);
+float euclideanDistance(const Position2D&, const Position2D&);
 
 /**
  * Compute the Chebyshev distance between two 2D positions.
  */
-double chebyshevDistance(const Position2D&, const Position2D&);
+float chebyshevDistance(const Position2D&, const Position2D&);
 
 /**
  * Compute the Chebyshev distance between two 2D positions.
  */
-double octileDistance(const Position2D&, const Position2D&);
+float octileDistance(const Position2D&, const Position2D&);
 }  // namespace lz
